8b41550de983871e6838b98f599ef0417ba01ea0
DigitalCreativityapps <apps@el-abda3.com>
2022-11-21 17:52:28 +0200

    add cli post for xcode cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  7usoomat
+#
+#  Created by George Awad on 26/07/2022.
+#
+brew install cocoapods
+pod install
---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  7usoomat
+#
+#  Created by George Awad on 26/07/2022.
+#  
